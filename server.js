f9150d02f8fc3e78e8b10f03a376dad8e605860f
MICHAEL AJIBADE <mikeal18@yahoo.co.uk>
2020-10-08 01:11:42 -0400

    commit

--- server.js
+++ server.js
@@ -16,11 +16,16 @@ app.use(express.static('public'));
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+
     res.send(`Lab 5 for ${process.env.NAME}`);
+
+    res.send('Hello World');
+
   })
   .post((req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
+    res.send('Hello World');
   });
 
 app.listen(port, () => {
